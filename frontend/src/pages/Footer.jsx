78165af5f3ab2cd73d65a46f6571df4c2df53f59
THElib03 <theliberator03@gmail.com>
2026-05-18 01:47:47 +0200

    (fix) footer: improved links readability

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -6,13 +6,16 @@ const Footer = () => {
             <div className="w-full! mx-auto px-8 text-center">
                 <p className="text-slate-300">&copy; 2026 Saal Booking System. Copyleft under <a href="https://www.gnu.org/licenses/gpl-3.0.html" className="text-slate-300 no-underline">GPL 3.0 License</a>.</p>
                 <div className="flex justify-center gap-8 mt-4">
-                    <a href="#" className="text-slate-300 no-underline">
+                    <a href="https://github.com/THElib03/proyecto" className="footer-link">
+                        Saal's GitHub
+                    </a>
+                    <a href="#" className="footer-link">
                         Privacy Policy
                     </a>
-                    <a href="#" className="text-slate-300 no-underline">
+                    <a href="#" className="footer-link">
                         Terms of Service
                     </a>
-                    <a href="#" className="text-slate-300 no-underline">
+                    <a href="mailto:marinvilchesmartin@gmail.com" className="footer-link">
                         Contact Us
                     </a>
                 </div>
